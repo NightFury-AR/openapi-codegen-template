51e7362e9f5a55fc93ec9b4fb57b0a60bb580b62
NightFury <aashiqrahmanu@gmail.com>
2023-08-31 16:12:17 +0530

    bkp

---
+++ src/main/java/com/example/sampleopenapicodegen/SampleOpenapiCodegenApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleopenapicodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOpenapiCodegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleOpenapiCodegenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleopenapicodegen/SampleOpenapiCodegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleopenapicodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleOpenapiCodegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
